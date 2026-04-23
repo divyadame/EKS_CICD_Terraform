45647a9b4f7785c33d3627e6ad0490bdc3c45e81
Divya <divyadama450@gmail.com>
2026-04-23 13:19:52 +0000

    added few lines to fix public access and access entries

--- eks.tf
+++ eks.tf
@@ -1,4 +1,3 @@
-# 2. Use the official EKS module for efficiency
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
@@ -6,29 +5,23 @@ module "eks" {
   cluster_name    = "my-low-cost-cluster"
   cluster_version = "1.30"
 
-  # Use default VPC for simplicity in testing
+  # DISABLE/REMOVE Auto Mode settings
+  # cluster_compute_config = { enabled = true } <-- REMOVE THIS
+  # create_auto_mode_iam_resources = true      <-- REMOVE THIS
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+  enable_cluster_creator_admin_permissions = true
+
   vpc_id     = "vpc-091918d9aab3e8b40"
   subnet_ids = ["subnet-01ef067e5b989506f", "subnet-061628f2ae1db0ee3"]
+  cluster_endpoint_public_access  = true 
 
-  cluster_compute_config = {
-    enabled = false
-  }
-  tags = {
-    Environment = "dev"
-    Project     = "learning-eks"
-    ManagedBy   = "terraform"
-  }
-
-  # Configure managed node groups with Free Tier eligible instances
   eks_managed_node_groups = {
     free_tier_nodes = {
-      instance_types = ["t3.micro"] # Free tier eligible in many regions
+      instance_types = ["t3.micro"]
       min_size       = 1
       max_size       = 2
       desired_size   = 1
-
-      # OPTIONAL: Use Spot instances to save up to 90% (Not Free Tier, but cheaper)
-      capacity_type = "SPOT"
+      capacity_type  = "SPOT"
     }
   }
 }
